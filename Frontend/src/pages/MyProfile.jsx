bc5dfc2b55df89f640ba956a8d27cdf74135a4be
aditya-raulji <-email@example.com>
2025-03-19 18:52:53 +0530

    Add initial MyProfile component with static user data and basic layout

---
+++ Frontend/src/pages/MyProfile.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from 'react'
+import { assets } from '../assets/assets'
+import { FaPhone, FaEnvelope, FaMapMarkerAlt, FaEdit, FaSave } from 'react-icons/fa'; // Adding icons for better UI
+
+const MyProfile = () => {
+
+
+  const [isEdit, setIsEdit] = useState(false)
+
+  return (
+    <div className='max-w-lg flex flex-col gap-4 text-sm bg-white p-6 rounded-lg shadow-lg'>
+      {/* Profile Image */}
+      <div className="flex justify-center">
+        <img className='w-36 h-36 rounded-full border-4 border-pink-300' src={userData.image} alt="" />
+      </div>
+
+      {/* Name */}
+      <div className="flex justify-between items-center mt-4">
+        {
+          isEdit
+            ? <input className='bg-gray-50 text-3xl font-medium max-w-[60%] mt-4 p-2 rounded-md border-2 border-pink-400 focus:outline-none' type="text" value={userData.name} onChange={e => setUserData(prev => ({ ...prev, name: e.target.value }))} />
+            : <p className='font-medium text-3xl text-pink-600'>{userData.name}</p>
+        }
+      </div>
+
+      <hr className='bg-zinc-400 h-[1px] border-none' />
+
+  
+
+
+      
+    </div>
+  )
+}
+
+export default MyProfile;
