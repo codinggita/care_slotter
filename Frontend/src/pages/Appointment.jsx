429ad1338167da1c97c5994ba2d586e34157fde2
aditya-raulji <-email@example.com>
2025-03-21 09:11:42 +0530

    Enhance UI with Framer Motion animations for doctor card and slots

--- Frontend/src/pages/Appointment.jsx
+++ Frontend/src/pages/Appointment.jsx
@@ -20,6 +20,40 @@ const Appointment = () => {
   const getAvailableSlots = async () => {
     setDocSlots([]);
 
+    let today = new Date();
+
+    for (let i = 0; i < 7; i++) {
+      let currenDate = new Date(today);
+      currenDate.setDate(today.getDate() + i);
+
+      let endTime = new Date();
+      endTime.setDate(today.getDate() + i);
+      endTime.setHours(21, 0, 0, 0);
+
+      if (today.getDate() === currenDate.getDate()) {
+        currenDate.setHours(currenDate.getHours() > 10 ? currenDate.getHours() + 1 : 10);
+        currenDate.setMinutes(currenDate.getMinutes() > 30 ? 30 : 0);
+      } else {
+        currenDate.setHours(10);
+        currenDate.setMinutes(0);
+      }
+
+      let timeSlots = [];
+
+      while (currenDate < endTime) {
+        let formattedTime = format(currenDate, 'HH:mm'); // Use date-fns to format the time
+
+        timeSlots.push({
+          datetime: new Date(currenDate),
+          time: formattedTime,
+        });
+
+        currenDate.setMinutes(currenDate.getMinutes() + 30);
+      }
+
+      setDocSlots((prev) => [...prev, timeSlots]);
+    }
+  };
 
 
 
